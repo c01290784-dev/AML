96a1813e3257e3775638e46d87aba792df1faecf
Karthik <c01290784@gmail.com>
2026-01-08 19:02:27 +0530

    first commit

---
+++ LAB_8_1_26/task1.py
@@ -0,0 +1,8 @@
+from data import generate_xor_data
+from visual import plot_2d_data
+def main():
+  X,y=generate_xor_data()
+  plot_2d_data(X,y, title="NON Linearly Separable Data")
+
+if __name__=="__main__":
+  main()
